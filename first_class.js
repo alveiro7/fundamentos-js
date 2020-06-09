f4c90f75f84458aba5f5669b11a2bc922cf8d27e
Hoyos Lozano <Alveiro.Hoyos@cencosud.com.co>
2020-06-09 14:28:10 -0500

    Mi primer commit Platzi Master

---
+++ first_class.js
@@ -0,0 +1,11 @@
+// Imprimir un mensaje en consola.
+console.log('Hola mundo JS'); 
+
+// Declarar variable.
+var nombre; 
+
+nombre='Alveiro';
+
+// Imprime en la consola concatenando texto y variable.
+console.log('Hola ' + nombre) 
+
---
+++ second_class.js
@@ -0,0 +1,16 @@
+// Declaracion variables strings
+var nombre='Alveiro', apellido='Hoyos'
+
+// toUpperCase, que sirve para transformar un string a mayusculas.
+var nombreEnMayusculas = nombre.toUpperCase()
+
+// toLowerCase, que sirve para transformar un string a minusculas.
+var apellidoEnMinusculas = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+
+var cantidadDeLetrasDelNombre = nombre.length
+
+var nombreCompleto = `${nombre} ${apellido.toLocaleUpperCase}`
+
+var str = nombre.substr(1,2)
