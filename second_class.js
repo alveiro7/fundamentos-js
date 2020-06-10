7977dd5c5e9b6c65cac5b5358ce6bf212d9a9f6e
Hoyos Lozano <Alveiro.Hoyos@cencosud.com.co>
2020-06-09 21:57:53 -0500

    Terminando la clase dos

--- second_class.js
+++ second_class.js
@@ -1,16 +1,33 @@
 // Declaracion variables strings
 var nombre='Alveiro', apellido='Hoyos'
+console.log('Nombre completo --> '+ nombre + ' ' + apellido)
 
 // toUpperCase, que sirve para transformar un string a mayusculas.
 var nombreEnMayusculas = nombre.toUpperCase()
+console.log('Nombre en mayuscular --> '+ nombreEnMayusculas)
 
 // toLowerCase, que sirve para transformar un string a minusculas.
 var apellidoEnMinusculas = apellido.toLowerCase()
+console.log('Apellido en minusculas --> ' + apellidoEnMinusculas)
 
+
+// El método charAt() de String devuelve en un nuevo String el carácter UTF-16 de una cadena.
 var primeraLetraDelNombre = nombre.charAt(0)
+console.log('Primera letra del nombre --> '+ primeraLetraDelNombre)
 
+// La propiedad length de un objeto String representa la longitud de una cadena, en unidades de código UTF-16.
 var cantidadDeLetrasDelNombre = nombre.length
+console.log('Cantidad de letras del nombre --> ' + cantidadDeLetrasDelNombre)
 
-var nombreCompleto = `${nombre} ${apellido.toLocaleUpperCase}`
 
+// Interporlacion de string, otra forma de concatenar sring.
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
+console.log(`Nombre completo --> ${nombreCompleto} cantidad de letras del nombre ${nombreCompleto.length}`)
+
+// El método substr() devuelve los caracteres de una cadena que comienzan en una localización especificada y de acuerdo al número de caracteres que se especifiquen.
 var str = nombre.substr(1,2)
+console.log(`Los caracteres 1 y 2 del nombre --> ${str}`)
+
+//Desafío: Encuentra la última letra de su nombre
+var str1 = nombre.substr(-1)
+console.log(`La ultima letra del nombre es --> ${str1}`)
